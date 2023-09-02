a16772b291e4c26b511e45e8882ececb1124bc1d
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-09-02 16:54:04 -0400

    Updated logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import gatorLogo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p>
+          My name is Natasha and this is our team project.
+        </p>	
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
